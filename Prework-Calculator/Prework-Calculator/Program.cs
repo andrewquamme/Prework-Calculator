74186527428838a22b4ab7ac205e8aa6663cd432
andrewquamme <andrewquamme@gmail.com>
2019-01-07 12:31:04 -0800

    Comments code

--- Prework-Calculator/Prework-Calculator/Program.cs
+++ Prework-Calculator/Prework-Calculator/Program.cs
@@ -6,9 +6,11 @@ namespace Prework_Calculator
     {
         static void Main(string[] args)
         {
+            //Main flow control
             int userSelection = 0;
             while(userSelection != 5)
             {
+                //Keep displaying the menu after each calculation until user exits
                 float num1 = 0;
                 float num2 = 0;
 
@@ -16,6 +18,7 @@ namespace Prework_Calculator
 
                 if(userSelection != 5)
                 {
+                    //Get two numbers from the user
                     Console.Write("Enter the first number: ");
                     num1 = float.Parse(Console.ReadLine());
                     Console.Write("Enter the second number: ");
@@ -36,6 +39,7 @@ namespace Prework_Calculator
                     case 4:
                         if(num2 == 0)
                         {
+                            //Handle division by zero
                             Console.WriteLine("Cannot divide by zero!");
                             Console.Write("Press any key to continue");
                             Console.ReadLine();
@@ -52,6 +56,7 @@ namespace Prework_Calculator
 
         static int ShowMenu()
         {
+            //Clear the screen, show the menu, and get user input for option selection
             Console.Clear();
             Console.WriteLine("Calculator!");
             Console.WriteLine("1: Addition");
@@ -65,27 +70,33 @@ namespace Prework_Calculator
 
         static void displayResult(float num1, float num2, string calcOperator, float result)
         {
+            //Take in the two numbers entered by the user, the operator used, and the result
+            //and display them in a calulator-like way (2 + 2 = 4)
             Console.WriteLine("{0} {1} {2} = {3}", num1, calcOperator, num2, result);
             Console.ReadLine();
         }
 
         static float Add(float num1, float num2)
         {
+            // Takes two numbers and returns addition of the two
             return num1 + num2;
         }
 
         static float Subtract(float num1, float num2)
         {
+            // Takes two numbers and returns subtraction of the two
             return num1 - num2;
         }
 
         static float Multiply(float num1, float num2)
         {
+            // Takes two numbers and returns multiplication of the two
             return num1 * num2;
         }
 
         static float Divide(float num1, float num2)
         {
+            // Takes two numbers and returns division of the two
             return num1 / num2;
         }
     }
